f1537a19672094a5fdc14c5acbe3722c33092b49
Michael Scott <myscott1996@gmail.com>
2024-10-05 15:26:16 -0500

    Refactor board handlers

--- web/games/camelot/scripts/board.js
+++ web/games/camelot/scripts/board.js
@@ -10,27 +10,8 @@ export default class Board {
 
         this.canvas = canvas;
         this.canvas.ready().then(() => {
-            this.canvas.addEventListener('mousemove', (e) => {
-
-                const row = Math.floor((e.offsetY + 1) / this.canvas.canvas.offsetHeight * this.rows);
-                const col = Math.floor((e.offsetX + 1) / this.canvas.canvas.offsetWidth * this.cols);
-
-                if (row === this.current?.row && col === this.current?.col) {
-                    return;
-                }
-
-                if (this.boardMask[row * this.cols + col]) {
-                    this.current = { row, col };
-                } else {
-                    this.current = null;
-                }
-
-                this.draw();
-            });
-            this.canvas.addEventListener('mouseout', (e) => {
-                this.current = null;
-                this.draw();
-            });
+            this.canvas.addEventListener('mousemove', (e) => { this.onMouseMove(e); });
+            this.canvas.addEventListener('mouseout', (e) => { this.onMouseOut(e); });
         });
 
         if (this.rows * this.cols !== boardMask.length) {
@@ -48,5 +29,35 @@ export default class Board {
 
     draw() {
         this.canvas.draw(this.boardState, this.current);
+    }
+
+    // helper methods for use in event listeners
+    calculateRowCol(offsetX, offsetY) {
+        const row = Math.floor((offsetY + 1) / this.canvas.canvas.offsetHeight * this.rows);
+        const col = Math.floor((offsetX + 1) / this.canvas.canvas.offsetWidth * this.cols);
+
+        return { row, col };
+    }
+
+    // define event listeners
+    onMouseMove(e) {
+        const { row, col } = this.calculateRowCol(e.offsetX, e.offsetY);
+
+        if (row === this.current?.row && col === this.current?.col) {
+            return;
+        }
+
+        if (this.boardMask[row * this.cols + col]) {
+            this.current = { row, col };
+        } else {
+            this.current = null;
+        }
+
+        this.draw();
+    }
+
+    onMouseOut(_e) {
+        this.current = null;
+        this.draw();
     }
 }
