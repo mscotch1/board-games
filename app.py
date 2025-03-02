ff589ac8376cded7088cc4a44a06cdef4918f680
Michael Scott <myscott1996@gmail.com>
2025-03-01 19:49:08 -0600

    Allow unsafe werkzeug
    Need this to use flask app, gunicorn breaks socket usage for now. May
    try to fix later?

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def serve_file(filename):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=True)
+    socketio.run(app, host="0.0.0.0", port=5000, use_reloader=True, allow_unsafe_werkzeug=True)
